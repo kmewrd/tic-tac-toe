eb0ffd5f41d5d1700ecbf7fa22f69b561c0d9c9c
Kim Ward <kmewrd@outlook.com>
2022-01-13 16:58:34 -0600

    Add currentGame variable and query selectors

---
+++ main.js
@@ -0,0 +1,5 @@
+var gameBoard = document.querySelector(".game-board");
+var turnTracker = document.querySelector(".turn-tracker");
+var playerLeftWins = document.querySelector(".player-left-wins");
+var playerRightWins = document.querySelector(".player-right-wins");
+var currentGame;
