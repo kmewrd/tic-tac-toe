cf9f4269031fd8fa73a5105f2c8cf59e3e92bb88
Kim Ward <kmewrd@outlook.com>
2022-01-13 18:02:58 -0600

    Write listener and handler that places tokens on DOM

--- main.js
+++ main.js
@@ -2,4 +2,13 @@ var gameBoard = document.querySelector(".game-board");
 var turnTracker = document.querySelector(".turn-tracker");
 var playerLeftWins = document.querySelector(".player-left-wins");
 var playerRightWins = document.querySelector(".player-right-wins");
-var currentGame;
+var currentGame = new Game();
+
+gameBoard.addEventListener('click', acceptToken);
+
+function acceptToken(e) {
+  if (!currentGame.board[e.target.id]) {
+    e.target.innerText = currentGame.turn.token;
+    currentGame.placeToken(e.target.id);
+  }
+}
