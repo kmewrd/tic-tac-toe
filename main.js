836e7f78f2a468411039a04a92b165f8eba50ac2
Kim Ward <kmewrd@outlook.com>
2022-01-13 20:55:16 -0600

    Add function that updates h2 with current turn or winner

--- main.js
+++ main.js
@@ -4,11 +4,23 @@ var playerLeftWins = document.querySelector(".player-left-wins");
 var playerRightWins = document.querySelector(".player-right-wins");
 var currentGame = new Game();
 
-gameBoard.addEventListener('click', acceptToken);
+gameBoard.addEventListener('click', function(e) {
+  acceptToken(e);
+  updateTurn();
+});
 
 function acceptToken(e) {
-  if (!currentGame.board[e.target.id]) {
+  if (!currentGame.winner && !currentGame.board[e.target.id]) {
     e.target.innerText = currentGame.turn.token;
     currentGame.placeToken(e.target.id);
+  }
+  console.log(currentGame.turn);
+}
+
+function updateTurn() {
+  if (!currentGame.winner) {
+    turnTracker.innerText = `It's ${currentGame.turn.token}'s turn!`;
+  } else {
+    turnTracker.innerText = `${currentGame.winner.token} is the winner!`;
   }
 }
