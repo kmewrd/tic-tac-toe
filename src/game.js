b25a9e817d8c44b804296617af096086df0436c9
Kim Ward <kmewrd@outlook.com>
2022-01-14 18:04:01 -0600

    Change player tokens to icon/image filepath

--- src/game.js
+++ src/game.js
@@ -1,7 +1,7 @@
 class Game {
   constructor() {
-    this.playerLeft = new Player("left", "👾");
-    this.playerRight = new Player("right", "🤖");
+    this.playerLeft = new Player("left", "./assets/x-icon.svg");
+    this.playerRight = new Player("right", "./assets/o-icon.svg");
     this.startingPlayer = this.playerLeft;
     this.turn = this.playerLeft;
     this.winner = null;
